4b263ba53d4cc9c5a9e3127fe7aea2ac238e957e
Juan <je.ortiz10003@gmail.com>
2024-01-14 18:56:50 -0500

    1. Creating ip-informer as a small proyect using vanilla js, html and a external API.

---
+++ app.js
@@ -0,0 +1,11 @@
+const OPTIONS = {
+    method: 'GET',
+    params: {
+      risk: 'true',
+      hostname: 'false'
+    },
+    headers: {
+      'X-RapidAPI-Key': 'a5efb59083msha4678c47e823d1dp182a10jsn79605db6f22d',
+      'X-RapidAPI-Host': 'ip-directory.p.rapidapi.com'
+    }
+  };
